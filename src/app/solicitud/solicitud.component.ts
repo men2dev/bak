49773d61eff7991d99c0214ddc52417bbe7267c3
men2dev <men2dev@gmail.com>
2020-01-23 19:18:47 +0100

    styles

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators
-} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-solicitud',
@@ -11,20 +7,14 @@ import {
   styleUrls: ['./solicitud.component.scss']
 })
 export class SolicitudComponent implements OnInit {
-<<<<<<< HEAD
-  solicitud: FormGroup ;
-=======
-  solicitud: FormGroup = this.fb.group({
-    nif: [null, Validators.required],
-    fechaNacimiento :[null, Validators.required],
-    codigoPostal:[null, Validators.required],
-    portal : [null, Validators.required]
-  });
->>>>>>> b703ff3586cff09356a787535d7e4e59a33491a5
+  solicitud: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.solicitud = fb.group({
-      nif: [null, Validators.required]
+      nif: [null, Validators.required],
+      fechaNacimiento: [null, Validators.required],
+      codigoPostal: [null, Validators.required],
+      portal: [null, Validators.required]
     });
   }
 
